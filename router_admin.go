39a38c4beced689a1c5cd560d62fe4ea5b22b6ea
huyshop <huyhoang2028dv@gmail.com>
2025-03-21 15:54:55 +0700

    update sign in admin

--- router_admin.go
+++ router_admin.go
@@ -3,6 +3,8 @@ package main
 func (r *Router) mappingRouterAdmin() {
 	r1 := r.route.Group("/admin")
 
+	r1.POST("/sign-in", r.handleSignInAdmin)
+
 	r1.GET("/user", r.handleGetListUser)
 	r1.POST("/user", r.handleCreateUser)
 	r1.GET("/user/:id", r.handleGetUser)
--- user.go
+++ user.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"sort"
+
 	"github.com/gin-gonic/gin"
 	"github.com/huyshop/api/utils"
+	permpb "github.com/huyshop/header/permission"
 	userpb "github.com/huyshop/header/user"
 )
 
@@ -12,6 +15,62 @@ type Response struct {
 	Data    interface{} `json:"data,omitempty"`
 }
 
+func (r *Router) handleSignInAdmin(ctx *gin.Context) {
+	c, cancel := utils.MakeContext(MAXTIMEREQ, nil)
+	defer cancel()
+	req := &userpb.User{}
+	ctx.ShouldBindJSON(req)
+	resp, err := r.userSer.SignIn(c, req)
+	if err != nil {
+		ctx.JSON(500, &Response{Code: -1, Message: err.Error()})
+		return
+	}
+	role, err := r.permSer.GetRole(c, &permpb.RoleRequest{Id: resp.GetUser().GetRoleId()})
+	if err != nil {
+		ctx.JSON(500, &Response{Code: -1, Message: err.Error()})
+		return
+	}
+	pages, err := r.permSer.ListPages(c, &permpb.PageRequest{RoleId: role.GetId()})
+	if err != nil {
+		ctx.JSON(500, &Response{Code: -1, Message: err.Error()})
+		return
+	}
+	// Tạo map để tra cứu nhanh
+	mapMenu := make(map[int32]*permpb.Page)
+	for _, p := range pages.Pages {
+		mapMenu[p.Id] = p
+	}
+
+	// Xây dựng cây menu
+	var menu []*permpb.Page
+	for _, p := range pages.Pages {
+		if p.ParentId == 0 {
+			menu = append(menu, p) // Trang chính
+		} else if parent, ok := mapMenu[p.ParentId]; ok {
+			parent.Children = append(parent.Children, p) // Thêm vào trang cha
+		}
+	}
+	// Sắp xếp menu chính và menu con
+	sortPages(menu)
+
+	// Gán lại quyền cho user
+	role.Permission = menu
+	resp.User.Role = role
+	resp.User.Permissions = menu
+	ctx.JSON(200, &Response{Code: 0, Message: "success", Data: resp})
+}
+
+func sortPages(pages []*permpb.Page) {
+	sort.Slice(pages, func(i, j int) bool {
+		return pages[i].Id < pages[j].Id // Sắp xếp theo ID tăng dần (hoặc theo Name, Order, ...)
+	})
+	for _, p := range pages {
+		if len(p.Children) > 0 {
+			sortPages(p.Children) // Đệ quy để sắp xếp các menu con
+		}
+	}
+}
+
 func (r *Router) handleGetListUser(ctx *gin.Context) {
 	c, cancel := utils.MakeContext(MAXTIMEREQ, nil)
 	defer cancel()
@@ -19,17 +78,10 @@ func (r *Router) handleGetListUser(ctx *gin.Context) {
 	utils.BindQuery(req, ctx)
 	users, err := r.userSer.ListUsers(c, req)
 	if err != nil {
-		ctx.JSON(500, Response{
-			Code:    -1,
-			Message: err.Error(),
-		})
+		ctx.JSON(500, &Response{Code: -1, Message: err.Error()})
 		return
 	}
-	ctx.JSON(200, &Response{
-		Code:    0,
-		Message: "success",
-		Data:    users,
-	})
+	ctx.JSON(200, &Response{Code: 0, Message: "success", Data: users})
 }
 
 func (r *Router) handleGetUser(ctx *gin.Context) {
@@ -38,17 +90,10 @@ func (r *Router) handleGetUser(ctx *gin.Context) {
 	id := ctx.Param("id")
 	user, err := r.userSer.GetUser(c, &userpb.UserRequest{Id: id})
 	if err != nil {
-		ctx.JSON(500, Response{
-			Code:    -1,
-			Message: err.Error(),
-		})
+		ctx.JSON(500, &Response{Code: -1, Message: err.Error()})
 		return
 	}
-	ctx.JSON(200, &Response{
-		Code:    0,
-		Message: "success",
-		Data:    user,
-	})
+	ctx.JSON(200, &Response{Code: 0, Message: "success", Data: user})
 }
 
 func (r *Router) handleCreateUser(ctx *gin.Context) {
@@ -58,16 +103,10 @@ func (r *Router) handleCreateUser(ctx *gin.Context) {
 	ctx.ShouldBindJSON(req)
 	_, err := r.userSer.CreateUser(c, req)
 	if err != nil {
-		ctx.JSON(500, Response{
-			Code:    -1,
-			Message: err.Error(),
-		})
+		ctx.JSON(500, &Response{Code: -1, Message: err.Error()})
 		return
 	}
-	ctx.JSON(200, &Response{
-		Code:    0,
-		Message: "success",
-	})
+	ctx.JSON(200, &Response{Code: 0, Message: "success"})
 }
 
 func (r *Router) handleUpdateUser(ctx *gin.Context) {
@@ -79,16 +118,10 @@ func (r *Router) handleUpdateUser(ctx *gin.Context) {
 	req.Id = id
 	_, err := r.userSer.UpdateUser(c, req)
 	if err != nil {
-		ctx.JSON(500, Response{
-			Code:    -1,
-			Message: err.Error(),
-		})
+		ctx.JSON(500, &Response{Code: -1, Message: err.Error()})
 		return
 	}
-	ctx.JSON(200, &Response{
-		Code:    0,
-		Message: "success",
-	})
+	ctx.JSON(200, &Response{Code: 0, Message: "success"})
 }
 
 func (r *Router) handleDeleteUser(ctx *gin.Context) {
@@ -97,14 +130,8 @@ func (r *Router) handleDeleteUser(ctx *gin.Context) {
 	id := ctx.Param("id")
 	_, err := r.userSer.DeleteUser(c, &userpb.User{Id: id})
 	if err != nil {
-		ctx.JSON(500, Response{
-			Code:    -1,
-			Message: err.Error(),
-		})
+		ctx.JSON(500, &Response{Code: -1, Message: err.Error()})
 		return
 	}
-	ctx.JSON(200, &Response{
-		Code:    0,
-		Message: "success",
-	})
+	ctx.JSON(200, &Response{Code: 0, Message: "success"})
 }
