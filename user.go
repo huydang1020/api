44a7516df3ee12fac24d0ad6b3f0826c247b5979
huyshop <huyhoang2028dv@gmail.com>
2025-04-10 16:28:32 +0700

    update

--- user.go
+++ user.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"strconv"
 
@@ -86,12 +87,40 @@ func (r *Router) handleCreateUser(ctx *gin.Context) {
 	c, cancel := utils.MakeContext(MAXTIMEREQ, nil)
 	defer cancel()
 	req := &userpb.User{}
-	req.FullName = ctx.PostForm("full_name")
-	req.Email = ctx.PostForm("email")
-	req.PhoneNumber = ctx.PostForm("phone_number")
-	req.Username = ctx.PostForm("username")
-	req.Password = ctx.PostForm("password")
-	req.RoleId = ctx.PostForm("role_id")
+	fullName := ctx.PostForm("full_name")
+	if fullName == "" {
+		utils.HandleError(LangMappingErr, ctx, errors.New("full_name is required"))
+		return
+	}
+	req.FullName = fullName
+
+	email := ctx.PostForm("email")
+	if email == "" {
+		utils.HandleError(LangMappingErr, ctx, errors.New(utils.E_not_found_email))
+		return
+	}
+	req.Email = email
+
+	username := ctx.PostForm("username")
+	if username == "" {
+		utils.HandleError(LangMappingErr, ctx, errors.New(utils.E_not_found_username))
+		return
+	}
+	req.Username = username
+
+	password := ctx.PostForm("password")
+	if password == "" {
+		utils.HandleError(LangMappingErr, ctx, errors.New(utils.E_not_found_password))
+		return
+	}
+	req.Password = password
+
+	roleId := ctx.PostForm("role_id")
+	if roleId == "" {
+		utils.HandleError(LangMappingErr, ctx, errors.New(utils.E_not_found_role_id))
+		return
+	}
+	req.RoleId = roleId
 	req.Province = ctx.PostForm("province")
 	req.District = ctx.PostForm("district")
 	req.Ward = ctx.PostForm("ward")
@@ -145,9 +174,55 @@ func (r *Router) handleUpdateUser(ctx *gin.Context) {
 	defer cancel()
 	id := ctx.Param("id")
 	req := &userpb.User{}
-	ctx.ShouldBindJSON(req)
 	req.Id = id
-	_, err := r.userSer.UpdateUser(c, req)
+	req.FullName = ctx.PostForm("full_name")
+	req.Email = ctx.PostForm("email")
+	req.PhoneNumber = ctx.PostForm("phone_number")
+	req.Username = ctx.PostForm("username")
+	req.Password = ctx.PostForm("password")
+	req.RoleId = ctx.PostForm("role_id")
+	req.Province = ctx.PostForm("province")
+	req.District = ctx.PostForm("district")
+	req.Ward = ctx.PostForm("ward")
+	req.State = ctx.PostForm("state")
+	req.Address = ctx.PostForm("address")
+	if birthday := ctx.PostForm("birthday"); birthday != "" {
+		birth, err := strconv.Atoi(birthday)
+		if err != nil {
+			log.Println("birthday err:", err)
+			utils.HandleError(LangMappingErr, ctx, err)
+			return
+		}
+		req.Birthday = int64(birth)
+	}
+
+	// Xử lý file avatar (nếu có)
+	form, err := ctx.MultipartForm()
+	if err == nil && form.File["avatar"] != nil {
+		avatar := []string{}
+		files := form.File["avatar"]
+		for _, file := range files {
+			imageName := file.Filename
+			image, err := file.Open()
+			if err != nil {
+				log.Println("file open err:", err)
+				continue
+			}
+			defer image.Close()
+
+			imageUrl, err := UploadImageToCloudinary(c, image, imageName)
+			if err != nil {
+				log.Println("upload img err:", err)
+				continue
+			}
+			avatar = append(avatar, imageUrl)
+		}
+		if len(avatar) > 0 {
+			req.Avatar = avatar[0]
+		}
+	}
+	log.Println("req:", req)
+	_, err = r.userSer.UpdateUser(c, req)
 	if err != nil {
 		utils.HandleError(LangMappingErr, ctx, err)
 		return
--- utils/errors.go
+++ utils/errors.go
@@ -9,4 +9,8 @@ const (
 	E_invalid_user       = "not_found_user"
 	E_invalid_role       = "not_found_role"
 	E_invalid_permission = "not_found_permission"
+	E_not_found_email    = "not_found_email"
+	E_not_found_username = "not_found_username"
+	E_not_found_password = "invalid_password"
+	E_not_found_role_id  = "invalid_role_id"
 )
